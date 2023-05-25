3b622f82d9245e8b257ebc156dc894e3460a5864
Sarah Dean <128323898+sarahadean@users.noreply.github.com>
2023-05-24 22:15:55 -0500

    tests passed init lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,25 @@
-#!/usr/bin/env python3
 
+##Object Initialization
 class Dog:
-    pass
+    def __init__(self, name, breed = "Mutt"):
+        self.name = name
+        self.breed = breed
+
+    def sit(self):
+        print("The dog is sitting.")
+
+    def get_adopted(self, owner_name):
+        self.owner = owner_name
+fido = Dog("Fido")
+fido.name
+#Fido
+
+fido.owner = "Sophie"
+#self.attribute = value
+fido.owner
+#Sophie
+
+fido.get_adopted("Sophie")
+print(fido.owner)
+
+fido.sit()
